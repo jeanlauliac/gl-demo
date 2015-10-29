b35c4fc6ab42effac50a9fa07ab72de8fd1d012e
Jean Lauliac <jean@lauliac.com>
2015-10-29 21:42:07 +0000

    Add element array

--- main.cpp
+++ main.cpp
@@ -22,22 +22,16 @@ static void keyCallback(GLFWwindow* window, int key, int scancode, int action, i
   }
 }
 
-GLfloat VERTEX_DATA[] = {
-  0.0, 0.0,
-  0.5, 0.0,
-  0.5, 0.5,
-
-  0.0, 0.0,
-  0.0, 0.5,
-  -0.5, 0.5,
-
-  0.0, 0.0,
-  -0.5, 0.0,
+GLfloat VERTEX_POSITIONS[] = {
   -0.5, -0.5,
-
-  0.0, 0.0,
-  0.0, -0.5,
   0.5, -0.5,
+  0.5, 0.5,
+  -0.5, 0.5,
+};
+
+GLuint INDICES[6] = {
+  0, 1, 2,
+  2, 3, 0
 };
 
 int main() {
@@ -66,6 +60,9 @@ int main() {
   GLuint vbo;
   glGenBuffers(1, &vbo);
 
+  GLuint eab;
+  glGenBuffers(1, &eab);
+
   glpp::Program program = (
     ds::loadAndLinkProgram("shaders/basic.vs", "shaders/basic.fs")
   );
@@ -86,23 +83,27 @@ int main() {
 
   // Allocate space and upload the data from CPU to GPU
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(VERTEX_DATA) + sizeof(colorData), nullptr, GL_STATIC_DRAW);
-  glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(VERTEX_DATA), VERTEX_DATA);
-  glBufferSubData(GL_ARRAY_BUFFER, sizeof(VERTEX_DATA), sizeof(colorData), colorData);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(VERTEX_POSITIONS) + sizeof(colorData), nullptr, GL_STATIC_DRAW);
+  glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(VERTEX_POSITIONS), VERTEX_POSITIONS);
+  glBufferSubData(GL_ARRAY_BUFFER, sizeof(VERTEX_POSITIONS), sizeof(colorData), colorData);
 
   GLint positionAttribute = program.getAttribLocation("position");
   glVertexAttribPointer(positionAttribute, 2, GL_FLOAT, GL_FALSE, 0, 0);
   glEnableVertexAttribArray(positionAttribute);
 
   GLint colorAttribute = program.getAttribLocation("color");
-  glVertexAttribPointer(colorAttribute, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid *)sizeof(VERTEX_DATA));
+  glVertexAttribPointer(colorAttribute, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid *)sizeof(VERTEX_POSITIONS));
   glEnableVertexAttribArray(colorAttribute);
+
+  // Transfer the data from indices to eab
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eab);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(INDICES), INDICES, GL_STATIC_DRAW);
 
   while (!window.shouldClose()) {
     glClear(GL_COLOR_BUFFER_BIT);
 
     glBindVertexArray(vao);
-    glDrawArrays(GL_TRIANGLES, 0, 12);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     window.swapBuffers();
     glfwPollEvents();

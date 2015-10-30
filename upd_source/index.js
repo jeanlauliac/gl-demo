316b2c60912069f7e4a236242d7dff2c8f84769e
Jean Lauliac <jean@lauliac.com>
2015-10-30 22:43:38 +0000

    upd cache

--- upd_source/index.js
+++ upd_source/index.js
@@ -1,18 +1,65 @@
 /* @flow */
-import {spawn} from 'child_process'
+import crypto from 'crypto'
 import glob from 'glob'
+import mkdirp from 'mkdirp'
+import path from 'path'
+import {spawn} from 'child_process'
+
+function sha1(data) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(data);
+  return shasum.digest('hex');
+}
+
+const UPD_CACHE_PATH = '.upd_cache'
 
 ;(() => {
-  const clang = spawn(
-    'clang++',
-    [
-      '-o', 'gl-demo', '-framework', 'OpenGL',
-      '-Wall', '-std=c++14', '-lglew', '-lglfw3',
-      'main.cpp'
-    ].concat(glob.sync('glfwpp/*.cpp'))
-     .concat(glob.sync('glpp/*.cpp'))
-     .concat(glob.sync('ds/*.cpp'))
-  )
-  clang.stdout.pipe(process.stdout)
-  clang.stderr.pipe(process.stderr)
+  mkdirp.sync(UPD_CACHE_PATH)
+  const sourceFilePaths = ['main.cpp']
+    .concat(glob.sync('glfwpp/*.cpp'))
+    .concat(glob.sync('glpp/*.cpp'))
+    .concat(glob.sync('ds/*.cpp'));
+  const hashes = new Set()
+  const compilationPairs = sourceFilePaths.map(sourceFilePath => {
+    const hash = sha1(sourceFilePath)
+    if (hashes.has(hash)) {
+      throw new Error('object file hash collision')
+    }
+    hashes.add(hash)
+    return {
+      sourceFilePath,
+      objectFilePath: path.join(UPD_CACHE_PATH, hash + '.o'),
+      depFilePath: path.join(UPD_CACHE_PATH, hash + '.d'),
+    }
+  })
+  Promise.all(compilationPairs.map(pair => {
+    return new Promise((resolve, reject) => {
+      const clang = spawn(
+        'clang++',
+        [
+          '-o', pair.objectFilePath, '-Wall', '-std=c++14', '-c',
+          pair.sourceFilePath, '-fcolor-diagnostics',
+          '-MMD', '-MF', pair.depFilePath,
+        ]
+      ).on('exit', (code) => {
+        clang.stdout.pipe(process.stdout)
+        clang.stderr.pipe(process.stderr)
+        if (code === 0) {
+          resolve()
+        }
+        reject(code)
+      })
+    })
+  })).then(() => {
+    const clang = spawn(
+      'clang++',
+      [
+        '-o', 'gl-demo', '-framework', 'OpenGL',
+        '-Wall', '-std=c++14', '-lglew', '-lglfw3',
+        '-fcolor-diagnostics',
+      ].concat(compilationPairs.map(pair => pair.objectFilePath))
+    )
+    clang.stdout.pipe(process.stdout)
+    clang.stderr.pipe(process.stderr)
+  })
 })()
--- upd_source/libs.js
+++ upd_source/libs.js
@@ -1,3 +1,6 @@
 declare module 'glob' {
   declare function sync(glob: string): Array<string>;
 }
+declare module 'mkdirp' {
+  declare function sync(path: string): void;
+}

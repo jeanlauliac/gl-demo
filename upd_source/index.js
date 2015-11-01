2cc42e986d7f0c8ccbe4a4e18ddc18392206c548
Jean Lauliac <jean@lauliac.com>
2015-11-01 09:49:43 +0000

    Small fixes

--- upd_source/index.js
+++ upd_source/index.js
@@ -2,25 +2,28 @@
 import crypto from 'crypto'
 import glob from 'glob'
 import mkdirp from 'mkdirp'
+import nopt from 'nopt'
 import path from 'path'
 import {spawn} from 'child_process'
 
 function sha1(data) {
-  var shasum = crypto.createHash('sha1');
-  shasum.update(data);
-  return shasum.digest('hex');
+  var shasum = crypto.createHash('sha1')
+  shasum.update(data)
+  return shasum.digest('hex')
 }
 
 const UPD_CACHE_PATH = '.upd_cache'
 
 ;(() => {
+  const opts = nopt({'verbose': Boolean})
+  const vspawn = opts.verbose ? verboseSpawn : spawn
   mkdirp.sync(UPD_CACHE_PATH)
   const sourceFilePaths = ['main.cpp']
     .concat(glob.sync('glfwpp/*.cpp'))
     .concat(glob.sync('glpp/*.cpp'))
     .concat(glob.sync('ds/*.cpp'));
   const hashes = new Set()
-  const compilationPairs = sourceFilePaths.map(sourceFilePath => {
+  const objects = sourceFilePaths.map(sourceFilePath => {
     const hash = sha1(sourceFilePath)
     if (hashes.has(hash)) {
       throw new Error('object file hash collision')
@@ -32,14 +35,14 @@ const UPD_CACHE_PATH = '.upd_cache'
       depFilePath: path.join(UPD_CACHE_PATH, hash + '.d'),
     }
   })
-  Promise.all(compilationPairs.map(pair => {
+  Promise.all(objects.map(object => {
     return new Promise((resolve, reject) => {
-      const clang = spawn(
+      const clang = vspawn(
         'clang++',
         [
-          '-o', pair.objectFilePath, '-Wall', '-std=c++14', '-c',
-          pair.sourceFilePath, '-fcolor-diagnostics',
-          '-MMD', '-MF', pair.depFilePath,
+          '-c', object.sourceFilePath, '-o', object.objectFilePath,
+          '-Wall', '-std=c++14', '-fcolor-diagnostics',
+          '-MMD', '-MF', object.depFilePath,
         ]
       ).on('exit', (code) => {
         clang.stdout.pipe(process.stdout)
@@ -51,15 +54,25 @@ const UPD_CACHE_PATH = '.upd_cache'
       })
     })
   })).then(() => {
-    const clang = spawn(
+    const clang = vspawn(
       'clang++',
       [
         '-o', 'gl-demo', '-framework', 'OpenGL',
         '-Wall', '-std=c++14', '-lglew', '-lglfw3',
         '-fcolor-diagnostics',
-      ].concat(compilationPairs.map(pair => pair.objectFilePath))
+      ].concat(objects.map(object => object.objectFilePath))
     )
     clang.stdout.pipe(process.stdout)
     clang.stderr.pipe(process.stderr)
-  })
+  }).catch(error => { throw error })
 })()
+
+function verboseSpawn(cmd, args) {
+  const argsStr = args.map(escapeShellArg).join(' ')
+  console.log(`${cmd} ${argsStr}`)
+  return spawn(cmd, args)
+}
+
+function escapeShellArg(arg) {
+  return arg.replace(/( )/, '\\$1')
+}
--- upd_source/libs.js
+++ upd_source/libs.js
@@ -1,6 +1,11 @@
 declare module 'glob' {
   declare function sync(glob: string): Array<string>;
 }
+
 declare module 'mkdirp' {
   declare function sync(path: string): void;
+}
+
+declare module 'nopt' {
+  declare function exports(opts: Object): Object;
 }

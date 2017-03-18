342eb27c1f570741a7ee30a01f6ad529042e48ec
Jean Lauliac <jean@lauliac.com>
2017-03-18 16:47:54 +0100

    refactor cpp files

--- build/upd2/src/lib/command_line_template.h
+++ build/upd2/src/lib/command_line_template.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <string>
 
 namespace upd {
 
@@ -73,27 +74,7 @@ void reify_command_line_arg(
   std::vector<std::string>& args,
   const command_line_template_variable variable_arg,
   const command_line_parameters& parameters
-) {
-  switch (variable_arg) {
-    case command_line_template_variable::input_files:
-      args.insert(
-        args.cend(),
-        parameters.input_files.cbegin(),
-        parameters.input_files.cend()
-      );
-      break;
-    case command_line_template_variable::output_files:
-      args.insert(
-        args.cend(),
-        parameters.output_files.cbegin(),
-        parameters.output_files.cend()
-      );
-      break;
-    case command_line_template_variable::dependency_file:
-      args.push_back(parameters.dependency_file);
-      break;
-  }
-}
+);
 
 /**
  * Specialize a command line template for a particular set of files.
@@ -101,18 +82,6 @@ void reify_command_line_arg(
 command_line reify_command_line(
   const command_line_template& base,
   const command_line_parameters& parameters
-) {
-  command_line result;
-  result.binary_path = base.binary_path;
-  for (auto const& part: base.parts) {
-    for (auto const& literal_arg: part.literal_args) {
-      result.args.push_back(literal_arg);
-    }
-    for (auto const& variable_arg: part.variable_args) {
-      reify_command_line_arg(result.args, variable_arg, parameters);
-    }
-  }
-  return result;
-}
+);
 
 }

c8b22530ae70064c77cced70f26bb1008f7228ea
Jean Lauliac <jean@lauliac.com>
2017-04-09 12:56:45 +0100

    no more crazy branching

--- build/upd2/src/lib/glob.cpp
+++ build/upd2/src/lib/glob.cpp
@@ -8,31 +8,65 @@ struct matcher {
     target(target),
     candidate(candidate) {}
 
+  /**
+   * A glob pattern is matched only if we can find a sequence of segments
+   * that matches, and if we reached the very end of the candidate doing so.
+   * If we don't, we still have a chance to recover by restoring the state
+   * to the last wildcard and trying again shifted by one character.
+   *
+   * Take for example, the pattern `foo*bar` and the candidate `foobarglobar`.
+   * At first we'd match `foobar`, but since it doesn't match the whole
+   * candidate, it's not correct. In that case we go back, and we consider the
+   * first "foo" as being matched by the wildcard instead of the literal.
+   */
   bool operator()() {
     clear();
-    start_new_segment();
-    while (true) {
-      bool does_match;
-      do {
-        does_match = match_literal(target[segment_ix].literal);
-      } while (!does_match && restore_wildcard());
-      if (!does_match) return false;
-
-      ++segment_ix;
-      if (segment_ix == target.size()) {
-        if (candidate_ix == candidate.size()) return true;
-        if (!restore_wildcard()) return false;
-        continue;
-      }
-      start_new_segment();
-    }
+    bool does_match, fully_matched;
+    do {
+      does_match = match_all_segments();
+      fully_matched = does_match && candidate_ix == candidate.size();
+    } while (does_match && !fully_matched && restore_wildcard());
+    return fully_matched;
   }
 
+  /**
+   * Reset everything to the beginning as we start the algo.
+   */
   void clear() {
     segment_ix = candidate_ix = bookmark_ix = last_wildcard_segment_ix = 0;
     has_bookmark = false;
   }
 
+  /**
+   * Try to match all of the known segments within the candidate. For each
+   * segment we match the literal. If it doesn't match, we may be able to
+   * go back and include more characters in a wildcard.
+   */
+  bool match_all_segments() {
+    bool does_match = true;
+    while (does_match && segment_ix < target.size()) {
+      start_new_segment();
+      do {
+        does_match = match_literal(target[segment_ix].literal);
+      } while (!does_match && restore_wildcard());
+      ++segment_ix;
+    }
+    return does_match;
+  }
+
+  void start_new_segment() {
+    if (target[segment_ix].prefix == placeholder::wildcard) start_wildcard();
+  }
+
+  void start_wildcard() {
+    bookmark_ix = candidate_ix;
+    last_wildcard_segment_ix = segment_ix;
+    has_bookmark = true;
+  }
+
+  /**
+   * Just match the literal character by character.
+   */
   bool match_literal(const std::string& literal) {
     size_t literal_ix = 0;
     while (
@@ -54,17 +88,6 @@ struct matcher {
     if (candidate_ix + target[segment_ix].literal.size() > candidate.size())
       return false;
     return true;
-  }
-
-  void start_wildcard() {
-    bookmark_ix = candidate_ix;
-    last_wildcard_segment_ix = segment_ix;
-    has_bookmark = true;
-  }
-
-  void start_new_segment() {
-    if (target[segment_ix].prefix == placeholder::wildcard)
-      start_wildcard();
   }
 
   const pattern& target;

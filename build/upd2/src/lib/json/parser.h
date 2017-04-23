c8a222ceb805b3cdeec64d065d6e517f91e5737b
Jean Lauliac <jean@lauliac.com>
2017-04-23 13:31:58 +0100

    Parse object with number

--- build/upd2/src/lib/json/parser.h
+++ build/upd2/src/lib/json/parser.h
@@ -5,8 +5,12 @@
 namespace upd {
 namespace json {
 
+template <typename Lexer, typename Handler, typename RetVal>
+RetVal parse_expression(Lexer& lexer, Handler& handler);
+
 struct unexpected_end_error {};
 struct unexpected_number_error {};
+struct unexpected_string_error {};
 struct unexpected_punctuation_error {};
 struct object_reader_invalid {};
 
@@ -33,16 +37,105 @@ private:
   std::string& field_name_;
 };
 
+struct read_new_field_name_handler {
+  read_new_field_name_handler(std::string& field_name): field_name_(field_name) {}
+
+  bool end() const { throw unexpected_end_error(); }
+
+  bool punctuation(punctuation_type type) const {
+    throw unexpected_punctuation_error();
+  }
+
+  bool string_literal(const std::string& literal) const {
+    field_name_ = literal;
+    return true;
+  }
+
+  bool number_literal(float literal) const {
+    throw unexpected_number_error();
+  }
+
+private:
+  std::string& field_name_;
+};
+
+struct post_field_handler {
+  bool end() const { throw unexpected_end_error(); }
+
+  bool punctuation(punctuation_type type) const {
+    if (type == punctuation_type::brace_close) return false;
+    if (type == punctuation_type::comma) return true;
+    throw unexpected_punctuation_error();
+  }
+
+  bool string_literal(const std::string& literal) const {
+    throw unexpected_string_error();
+  }
+
+  bool number_literal(float literal) const {
+    throw unexpected_number_error();
+  }
+};
+
+struct read_field_colon_handler {
+  void end() const { throw unexpected_end_error(); }
+
+  void punctuation(punctuation_type type) const {
+    if (type != punctuation_type::colon) {
+      throw unexpected_punctuation_error();
+    }
+  }
+
+  void string_literal(const std::string& literal) const {
+    throw unexpected_string_error();
+  }
+
+  void number_literal(float literal) const {
+    throw unexpected_number_error();
+  }
+};
+
+template <typename Lexer>
+struct field_value_reader {
+  field_value_reader(Lexer& lexer): lexer_(lexer), read_(false) {}
+
+  template <typename Handler, typename RetVal>
+  RetVal operator()(Handler& handler) {
+    read_ = true;
+    return parse_expression<Lexer, Handler, RetVal>(lexer_, handler);
+  }
+
+  bool read() const {
+    return read_;
+  }
+
+private:
+  Lexer& lexer_;
+  bool read_;
+};
+
 template <typename Lexer>
 struct object_reader {
   object_reader(Lexer& lexer): lexer_(lexer) {}
 
   template <typename FieldReader>
-  void operator()(FieldReader field_reader) {
+  void operator()(FieldReader read_field) {
     std::string field_name;
     read_field_name_handler rfn_handler(field_name);
-    if (lexer_.template next<read_field_name_handler, bool>(rfn_handler)) {
-      throw std::runtime_error("not implemented");
+    read_field_colon_handler rfc_handler;
+    bool has_field = lexer_.template next<read_field_name_handler, bool>(rfn_handler);
+    while (has_field) {
+      lexer_.template next<read_field_colon_handler, void>(rfc_handler);
+      field_value_reader<Lexer> read_field_value(lexer_);
+      read_field(field_name, read_field_value);
+      if (!read_field_value.read()) {
+        throw std::runtime_error("field value expression was not read");
+      }
+      post_field_handler pf_handler;
+      has_field = lexer_.template next<post_field_handler, bool>(pf_handler);
+      if (!has_field) continue;
+      read_new_field_name_handler rnfn_handler(field_name);
+      lexer_.template next<read_new_field_name_handler, bool>(rnfn_handler);
     }
   }
 

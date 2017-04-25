a01ae6233b3d4d0e1cfbce0f29838dcdc2f3bd06
Jean Lauliac <jean@lauliac.com>
2017-04-25 22:49:11 +0100

    remove deprecated JS upd tool

--- build/upd/src/create-freezed.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-
-'use strict';
-
-// Create an freezed object that has the specified prototype and values.
-export default function createFreezed<T>(prototype: Object, values: T): T {
-  return Object.freeze(Object.assign(Object.create(prototype), values));
-}

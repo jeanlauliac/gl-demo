c61f86fafb7de3ad6d5cb968e85f164d66d64315
Jean Lauliac <jean@lauliac.com>
2017-04-25 21:31:33 +0100

    add configure.js script

--- build/upd2/configure.js
+++ build/upd2/configure.js
@@ -1,19 +1,95 @@
 #!/usr/bin/env node
 
-const compile = [
-  'clang++ -c -o $out_file -Wall',
-  '-std=c++14 -fcolor-diagnostics -MMD -MF $dep_file',
-  '-I /usr/local/include $in_files',
-].join(' ');
+const updfile = require('./tools/lib/updfile');
 
-const link = [
-  'clang++ -o $out_files -Wall -std=c++14',
-  '-fcolor-diagnostics -L /usr/local/lib $in_files',
-].join(' ');
+const manifest = new updfile.Manifest();
 
-require('../updfile').export(upd => {
-  const sourceFiles = upd.glob('src/(**/*).cpp');
-  const objFiles = upd.rule(compile, sourceFiles, 'dist/$1.o');
-  const binaryFile = upd.rule(link, objFiles, 'upd');
-  upd.alias('all', binaryFile);
-});
+const compile_cpp_cli = manifest.cli_template('clang++', [
+  {literals: ["-c", "-o"], variables: ["output_file"]},
+  {
+    literals: ["-std=c++14", "-Wall", "-fcolor-diagnostics", "-MMD", "-MF"],
+    variables: ["dependency_file"]
+  },
+  {literals: ["-I", "/usr/local/include"], variables: ["input_files"]},
+]);
+
+const compile_c_cli = manifest.cli_template('clang++', [
+  {literals: ["-c", "-o"], variables: ["output_file"]},
+  {
+    literals: ["-x", "c", "-Wall", "-fcolor-diagnostics", "-MMD", "-MF"],
+    variables: ["dependency_file"]
+  },
+  {literals: ["-I", "/usr/local/include"], variables: ["input_files"]},
+]);
+
+const cppt_sources = manifest.source("(src/lib/**/*).cppt");
+
+const test_cpp_files = manifest.rule(
+  manifest.cli_template('tools/compile_test.js', [
+    {variables: ["input_files", "output_file", "dependency_file"]},
+  ]),
+  [cppt_sources],
+  "dist/($1).cpp"
+);
+
+const compiled_cpp_files = manifest.rule(
+  compile_cpp_cli,
+  [manifest.source("(src/lib/**/*).cpp")],
+  "dist/($1).o"
+);
+
+const compiled_c_files = manifest.rule(
+  compile_c_cli,
+  [manifest.source("(src/lib/**/*).c")],
+  "dist/($1).o"
+);
+
+const tests_cpp_file = manifest.rule(
+  manifest.cli_template('tools/index_tests.js', [
+    {variables: ["output_file", "dependency_file", "input_files"]}
+  ]),
+  [cppt_sources],
+  "dist/(tests).cpp"
+);
+
+const package_cpp_file = manifest.rule(
+  manifest.cli_template('tools/gen_package_info.js', [
+    {variables: ["output_file", "dependency_file", "input_files"]}
+  ]),
+  [manifest.source("package.json")],
+  "dist/(package).cpp"
+);
+
+const compiled_main_files = manifest.rule(
+  compile_cpp_cli,
+  [manifest.source("(src/main).cpp"), package_cpp_file],
+  "dist/($1).o"
+);
+
+const compiled_test_files = manifest.rule(
+  compile_cpp_cli,
+  [manifest.source("(tools/lib/testing).cpp"), test_cpp_files, tests_cpp_file],
+  "dist/($1).o"
+);
+
+const link_cpp_cli = manifest.cli_template('clang++', [
+  {literals: ["-o"], variables: ["output_file"]},
+  {
+    literals: ["-Wall", "-fcolor-diagnostics", "-std=c++14", "-L", "/usr/local/lib"],
+    variables: ["input_files"]
+  }
+]);
+
+manifest.rule(
+  link_cpp_cli,
+  [compiled_cpp_files, compiled_c_files, compiled_main_files],
+  "dist/upd"
+);
+
+manifest.rule(
+  link_cpp_cli,
+  [compiled_cpp_files, compiled_c_files, compiled_test_files],
+  "dist/unit_tests"
+);
+
+manifest.export(__dirname);

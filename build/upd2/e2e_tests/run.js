a74a230a479e83dd5605a2be64aee3588db79918
Jean Lauliac <jean@lauliac.com>
2017-04-27 23:08:25 +0100

    tentative e2e testing

--- build/upd2/e2e_tests/run.js
+++ build/upd2/e2e_tests/run.js
@@ -5,7 +5,23 @@
 const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const rimraf = require('rimraf');
+
+const TEST_DIR_PATH = path.resolve(__dirname, '.test_files');
+
+const TESTS = [
+  {
+    name: "Simple",
+    files: {
+      "updfile.js": ""
+    },
+  }
+];
 
 (function main() {
+  rimraf.sync(TEST_DIR_PATH);
+  fs.mkdir(TEST_DIR_PATH);
+  fs.mkdir(path.join(TEST_DIR_PATH, '');
+  fs.writeFileSync(path.resolve(TEST_DIR_PATH, 'updfile.json'), JSON.stringify({}));
   child_process.execSync(path.join(__dirname, '../dist/upd --all'));
 })();
